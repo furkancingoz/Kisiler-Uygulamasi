e6d6634af9cfbc4e79be5ac6f75e338b7942ccab
Furkan Cingöz <efcingoz@outlook.com>
2023-10-12 01:09:26 +0300

    cell oluşturduk ve ayarlarını yaptık

---
+++ Kisiler Uygulamasi/UI/Table View/KisilerHucre.swift
@@ -0,0 +1,23 @@
+//
+//  KisilerHucre.swift
+//  Kisiler Uygulamasi
+//
+//  Created by Furkan Cingöz on 12.10.2023.
+//
+
+import UIKit
+
+class KisilerHucre: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
